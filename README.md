04a1951052a0fa353062b4cac623e4d015d5d28e
Stivdee <stefan.carlssons@gmail.com>
2020-11-20 15:23:29 +0100

    I have added a random number function. Also a function that will return sentences

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ In this project I will create a rand message generator. It shall contain 3 diffe
 ##I will use##
 I will use JavaScript to completet this task.
 
-##I have added random words to the project##
+I have added random words to the arrays.
+
+I have created a function that will return a number.
+
+I have created a function that will create random sentences. It has a parameter. The parameter will decide how many sentences that will be created and returned.
--- randomMessage.js
+++ randomMessage.js
@@ -1,6 +1,6 @@
 const randFirstPart = [
     "I wish I was ",
-    "I could ",
+    "I could be ",
     "You could ",
     "I am ",
     "Who is currently "
@@ -21,3 +21,24 @@ const randThirdPart = [
     "...",
     "-",
 ];
+
+function getRandomNumber() {
+
+    const randomNumber = Math.floor(Math.random() * randFirstPart.length);
+    return randomNumber;
+};
+
+function howManySentences (numberOf) {
+        const sentence = [];
+
+
+        for (let i = 0; i < numberOf; i++) {
+            const first = randFirstPart[getRandomNumber()];
+            const second = randSecondPart[getRandomNumber()];
+            const third = randThirdPart[getRandomNumber()];
+            sentence.push(first, second, third, ' ');
+        }
+        return sentence.join('');
+};
+const testWord = howManySentences(2);
+console.log(testWord);
